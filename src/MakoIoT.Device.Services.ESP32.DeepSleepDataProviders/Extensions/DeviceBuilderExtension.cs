d818e43c998969eeca82e653b753c94ae9a4cc2c
Marcin Torba <torbamarcin@gmail.com>
2023-12-19 23:41:48 +0100

    Move DisableDeepSleepGpioPin to static configuration instead of runtime config file. (#15)

--- src/MakoIoT.Device.Services.ESP32.DeepSleepDataProviders/Extensions/DeviceBuilderExtension.cs
+++ src/MakoIoT.Device.Services.ESP32.DeepSleepDataProviders/Extensions/DeviceBuilderExtension.cs
@@ -6,9 +6,10 @@ namespace MakoIoT.Device.Services.ESP32.DeepSleepDataProviders.Extensions
 
     public static class DeviceBuilderExtension
     {
-        public static IDeviceBuilder AddDeepSleepDataProviders(this IDeviceBuilder builder)
+        public static IDeviceBuilder AddDeepSleepDataProviders(this IDeviceBuilder builder, short deepSleepDisableGpioPinNumber = -1)
         {
             builder.DeviceStarting += Builder_DeviceStarting;
+            builder.Services.AddSingleton(typeof(DeepSleepDataProviderConfiguration), new DeepSleepDataProviderConfiguration() { DisableDeepSleepGpioPin = deepSleepDisableGpioPinNumber });
             return builder;
         }
 
